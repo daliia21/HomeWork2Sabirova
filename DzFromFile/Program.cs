25604df5c5009d58b49e63c25ad742b40a457cc9
daliasabirova6@gmail.com <daliasabirova6@gmail.com>
2024-11-11 22:36:56 +0300

    Добавьте файлы проекта.

---
+++ DzFromFile/Program.cs
@@ -0,0 +1,131 @@
+﻿/*1.Выведите на экран информацию о каждом типе данных в виде:
+Тип данных – максимальное значение – минимальное значение*/
+
+Console.WriteLine("Задание 1");
+
+Console.WriteLine("Тип данных – Максимальное значение – Минимальное значение");
+
+Console.WriteLine($"byte    - {byte.MaxValue} - {byte.MinValue}");
+Console.WriteLine($"sbyte   - {sbyte.MaxValue} - {sbyte.MinValue}");
+Console.WriteLine($"short   - {short.MaxValue} - {short.MinValue}");
+Console.WriteLine($"ushort  - {ushort.MaxValue} - {ushort.MinValue}");
+Console.WriteLine($"int     - {int.MaxValue} - {int.MinValue}");
+Console.WriteLine($"uint    - {uint.MaxValue} - {uint.MinValue}");
+Console.WriteLine($"long    - {long.MaxValue} - {long.MinValue}");
+Console.WriteLine($"ulong   - {ulong.MaxValue} - {ulong.MinValue}");
+Console.WriteLine($"float   - {float.MaxValue} - {float.MinValue}");
+Console.WriteLine($"double  - {double.MaxValue} - {double.MinValue}");
+Console.WriteLine($"decimal - {decimal.MaxValue} - {decimal.MinValue}");
+
+/*2.Напишите программу, в которой принимаются данные пользователя в виде имени,
+города, возраста и PIN-кода. Далее сохраните все значение в соответствующей
+переменной, а затем распечатайте всю информацию в правильном формате.*/
+
+Console.WriteLine("Задание 2");
+
+Person person = new Person();
+Console.WriteLine("Введите свое имя:");
+person.name = Console.ReadLine();
+Console.WriteLine("Введите ваш город:");
+person.town = Console.ReadLine();
+Console.WriteLine("Введите ваш возраст:");
+person.age = Convert.ToInt32( Console.ReadLine() );
+Console.WriteLine("Введите ваш PIN:");
+person.pin = Convert.ToInt32( Console.ReadLine() );
+person.Print();
+
+
+
+/*3.Преобразуйте входную строку: строчные буквы замените на заглавные, заглавные – на
+строчные.*/
+
+Console.WriteLine("Задание 3");
+
+Console.WriteLine("Введите строку:");
+string input = Console.ReadLine(); 
+
+Invert invert = new Invert();
+invert.line = input;
+invert.Print();
+
+
+
+/*4.Введите строку, введите подстроку. Необходимо найти количество вхождений и вывести
+на экран.*/
+
+Console.WriteLine("Задание 4");
+
+/*5.Цель этой задачи - определить, сколько бутылок виски беспошлинной торговли вам
+нужно будет купить, чтобы экономия по сравнению с обычной средней ценой фактически
+покрыла расходы на ваш отпуск. Вам будет предоставлена стандартная цена (normPrice),
+скидка в Duty Free (salePrice) и стоимость отпуска (holidayPrice). Например, если бутылка
+обычно стоит 10 фунтов стерлингов, а скидка в дьюти фри составляет 10%, вы
+сэкономите 1 фунт стерлингов на каждой бутылке. Если ваш отпуск стоит 500 фунтов
+стерлингов, ответ, который вы должны вернуть, будет 500. Все входные данные будут
+целыми числами. Пожалуйста, верните целое число. Округлить в меньшую сторону.*/
+
+Console.WriteLine("Задание 5");
+
+
+
+/*6.Создать структуру студента. У студента есть Фамилия, Имя, его Идентификатор, Дата
+рождения, Категория алкоголизма (a – студент алкоголик, b – студент любитель
+выпить, но не алкоголик, c – студент пьет по праздникам, d – студент не пьет), также у
+студента есть Объем выпитой жидкости конкретного напитка. Создать 5 студентов с
+различными параметрами. Посчитать общий объем выпитого, общий объем алкоголя
+(процент спирта) и кто сколько процентов алкоголя и жидкости от общего количества
+выпил. Предполагается, что студент пьет один вид напитка. Напитки задать в виде
+структуры: наименование типа напитка и процент спирта.*/
+
+Console.WriteLine("Задание 6");
+
+struct Student
+{
+    public string surName;
+    public string name;
+    public int ID;
+    public string birthDate;
+    public string categoryOfAlcoholism;
+    public int alcoholVolume;
+
+}
+
+
+
+
+struct Person
+{
+    public string name;
+    public string town;
+    public int age;
+    public int pin;
+    public void Print()
+    {
+        Console.WriteLine($"Имя - {name}, город - {town}, возраст - {age}, пин-код - {pin}");
+    }
+}
+
+struct Invert
+{
+    public string line;
+    public void Print()
+    {
+        char[] chars = line.ToCharArray();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (char.IsUpper(chars[i]))
+            {
+                chars[i] = char.ToLower(chars[i]);
+            }
+            else if (char.IsLower(chars[i]))
+            {
+                chars[i] = char.ToUpper(chars[i]);
+            }
+            Console.Write(chars[i]);
+        }
+
+
+    }
+
+}
---
+++ HomeWork2/Program.cs
@@ -0,0 +1,81 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+/*Упражнение 3.1 Создать перечислимый тип данных отображающий виды банковского
+счета (текущий и сберегательный). Создать переменную типа перечисления, присвоить ей
+значение и вывести это значение на печать.*/
+
+//Console.WriteLine("Упражнение 3.1");
+
+
+BankAccount type = BankAccount.Savings;
+
+Console.WriteLine($"Вид банковского счета - {type}");
+
+
+/*Упражнение 3.2 Создать структуру данных, которая хранит информацию о банковском
+счете – его номер, тип и баланс. Создать переменную такого типа, заполнить структуру
+значениями и напечатать результат.*/
+
+//Console.WriteLine("Упражнение 3.2");
+
+
+
+BankAcc acc = new BankAcc();
+acc.numberAcc = 3445678954347865;
+acc.typeAcc = "Сберегательный";
+acc.balanceAcc = 3452;
+acc.Print();
+
+
+
+
+/*Домашнее задание 3.1 Создать перечислимый тип ВУЗ{КГУ, КАИ, КХТИ}. Создать
+структуру работник с двумя полями: имя, ВУЗ. Заполнить структуру данными и
+распечатать.*/
+
+//Console.WriteLine("Упражнение 3.1");
+
+
+
+
+employeeProfile newProfile = new employeeProfile();
+newProfile.name = "Dalia";
+newProfile.VUZ = VUZ.KGU;
+newProfile.Print();
+
+
+
+struct BankAcc
+{
+    public long numberAcc;
+    public string typeAcc;
+    public double balanceAcc;
+    public void Print()
+    {
+        Console.WriteLine($"Номер счета - {numberAcc}, тип счета - {typeAcc}, баланс счета - {balanceAcc}");
+    }
+}
+
+enum BankAccount
+{
+    Сurrent,
+    Savings
+}
+
+struct employeeProfile
+{
+    public string name;
+    public VUZ VUZ;
+    public void Print()
+    {
+        Console.WriteLine($"Имя работника - {name}, Вуз - {VUZ}");
+    }
+}
+
+enum VUZ
+{
+    KGU,
+    KAI,
+    KHTI
+}
