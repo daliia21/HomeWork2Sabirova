07292fcbcc47ee87c36ea5c4bccca02fba10aef2
daliasabirova6@gmail.com <daliasabirova6@gmail.com>
2024-11-14 19:41:58 +0300

    раскоментила части кода

--- DzFromFile/Program.cs
+++ DzFromFile/Program.cs
@@ -1,52 +1,52 @@
 ﻿/*1.Выведите на экран информацию о каждом типе данных в виде:
 Тип данных – максимальное значение – минимальное значение*/
 
-//Console.WriteLine("Задание 1");
+Console.WriteLine("Задание 1");
 
-//Console.WriteLine("Тип данных – Максимальное значение – Минимальное значение");
+Console.WriteLine("Тип данных – Максимальное значение – Минимальное значение");
 
-//Console.WriteLine($"byte    - {byte.MaxValue} - {byte.MinValue}");
-//Console.WriteLine($"sbyte   - {sbyte.MaxValue} - {sbyte.MinValue}");
-//Console.WriteLine($"short   - {short.MaxValue} - {short.MinValue}");
-//Console.WriteLine($"ushort  - {ushort.MaxValue} - {ushort.MinValue}");
-//Console.WriteLine($"int     - {int.MaxValue} - {int.MinValue}");
-//Console.WriteLine($"uint    - {uint.MaxValue} - {uint.MinValue}");
-//Console.WriteLine($"long    - {long.MaxValue} - {long.MinValue}");
-//Console.WriteLine($"ulong   - {ulong.MaxValue} - {ulong.MinValue}");
-//Console.WriteLine($"float   - {float.MaxValue} - {float.MinValue}");
-//Console.WriteLine($"double  - {double.MaxValue} - {double.MinValue}");
-//Console.WriteLine($"decimal - {decimal.MaxValue} - {decimal.MinValue}");
+Console.WriteLine($"byte    - {byte.MaxValue} - {byte.MinValue}");
+Console.WriteLine($"sbyte   - {sbyte.MaxValue} - {sbyte.MinValue}");
+Console.WriteLine($"short   - {short.MaxValue} - {short.MinValue}");
+Console.WriteLine($"ushort  - {ushort.MaxValue} - {ushort.MinValue}");
+Console.WriteLine($"int     - {int.MaxValue} - {int.MinValue}");
+Console.WriteLine($"uint    - {uint.MaxValue} - {uint.MinValue}");
+Console.WriteLine($"long    - {long.MaxValue} - {long.MinValue}");
+Console.WriteLine($"ulong   - {ulong.MaxValue} - {ulong.MinValue}");
+Console.WriteLine($"float   - {float.MaxValue} - {float.MinValue}");
+Console.WriteLine($"double  - {double.MaxValue} - {double.MinValue}");
+Console.WriteLine($"decimal - {decimal.MaxValue} - {decimal.MinValue}");
 
 /*2.Напишите программу, в которой принимаются данные пользователя в виде имени,
 города, возраста и PIN-кода. Далее сохраните все значение в соответствующей
 переменной, а затем распечатайте всю информацию в правильном формате.*/
 
-//Console.WriteLine("Задание 2");
+Console.WriteLine("Задание 2");
 
-//Person person = new Person();
-//Console.WriteLine("Введите свое имя:");
-//person.name = Console.ReadLine();
-//Console.WriteLine("Введите ваш город:");
-//person.town = Console.ReadLine();
-//Console.WriteLine("Введите ваш возраст:");
-//person.age = Convert.ToInt32( Console.ReadLine() );
-//Console.WriteLine("Введите ваш PIN:");
-//person.pin = Convert.ToInt32( Console.ReadLine() );
-//person.Print();
+Person person = new Person();
+Console.WriteLine("Введите свое имя:");
+person.name = Console.ReadLine();
+Console.WriteLine("Введите ваш город:");
+person.town = Console.ReadLine();
+Console.WriteLine("Введите ваш возраст:");
+person.age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ваш PIN:");
+person.pin = Convert.ToInt32(Console.ReadLine());
+person.Print();
 
 
 
 /*3.Преобразуйте входную строку: строчные буквы замените на заглавные, заглавные – на
 строчные.*/
 
-//Console.WriteLine("Задание 3");
+Console.WriteLine("Задание 3");
 
-//Console.WriteLine("Введите строку:");
-//string input = Console.ReadLine(); 
+Console.WriteLine("Введите строку:");
+string input = Console.ReadLine();
 
-//Invert invert = new Invert();
-//invert.line = input;
-//invert.Print();
+Invert invert = new Invert();
+invert.line = input;
+invert.Print();
 
 
 
@@ -54,36 +54,36 @@
 на экран.*/
 
 
-//Console.WriteLine("Задание 4");
+Console.WriteLine("Задание 4");
 
-//Console.WriteLine("Введите строку:");
-//string str = Console.ReadLine();
+Console.WriteLine("Введите строку:");
+string str = Console.ReadLine();
 
-//Console.WriteLine("Введите подстроку:");
-//string substr = Console.ReadLine();
+Console.WriteLine("Введите подстроку:");
+string substr = Console.ReadLine();
 
-//int cntOccurrences = 0;
+int cntOccurrences = 0;
 
-//for (int i = 0; i < str.Length - substr.Length + 1; i++)
-//{
-//    bool flag = true;
+for (int i = 0; i < str.Length - substr.Length + 1; i++)
+{
+    bool flag = true;
 
-//    for (int j = 0; j < substr.Length; j++)
-//    {
-//        if (str[i + j] != substr[j])
-//        {
-//            flag = false;
-//            break;
-//        }
-//    }
+    for (int j = 0; j < substr.Length; j++)
+    {
+        if (str[i + j] != substr[j])
+        {
+            flag = false;
+            break;
+        }
+    }
 
-//    if (flag)
-//    {
-//        cntOccurrences++;
-//    }
-//}
+    if (flag)
+    {
+        cntOccurrences++;
+    }
+}
 
-//Console.WriteLine($"Количество вхождений подстроки: {cntOccurrences}");
+Console.WriteLine($"Количество вхождений подстроки: {cntOccurrences}");
 
 /*5.Цель этой задачи - определить, сколько бутылок виски беспошлинной торговли вам
 нужно будет купить, чтобы экономия по сравнению с обычной средней ценой фактически
@@ -94,30 +94,30 @@
 стерлингов, ответ, который вы должны вернуть, будет 500. Все входные данные будут
 целыми числами. Пожалуйста, верните целое число. Округлить в меньшую сторону.*/
 
-//Console.WriteLine("Задание 5");
+Console.WriteLine("Задание 5");
 
-//Console.WriteLine("Введите стандартную цену бутылки:");
-//int normPrice = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите стандартную цену бутылки:");
+int normPrice = Convert.ToInt32(Console.ReadLine());
 
-//Console.WriteLine("Введите величину скидки:");
-//int salePrice = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите величину скидки:");
+int salePrice = Convert.ToInt32(Console.ReadLine());
 
-//Console.WriteLine("Введите стоимость отпуска");
-//int holidayPrice = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите стоимость отпуска");
+int holidayPrice = Convert.ToInt32(Console.ReadLine());
 
 
-//if (salePrice > 0 && salePrice <= 100)
-//{
-//    double benefit = normPrice * (salePrice / 100.0);
+if (salePrice > 0 && salePrice <= 100)
+{
+    double benefit = normPrice * (salePrice / 100.0);
 
-//    int cntDays = Convert.ToInt32(Math.Ceiling(holidayPrice / benefit));
+    int cntDays = Convert.ToInt32(Math.Ceiling(holidayPrice / benefit));
 
-//    Console.WriteLine(cntDays);
-//}
-//else
-//{
-//    Console.WriteLine("Значение скидки не корректное");
-//}
+    Console.WriteLine(cntDays);
+}
+else
+{
+    Console.WriteLine("Значение скидки не корректное");
+}
 
 
 
@@ -214,14 +214,23 @@ Student student5 = new Student
 decimal volume = student1.alcoholVolume + student2.alcoholVolume + student3.alcoholVolume + student4.alcoholVolume + student5.alcoholVolume;
 Console.WriteLine($"Общий объём выпитого: {volume}");
 
-decimal volumeAlkohol = (student1.alcoholVolume * student1.alcoholicBeverage.alcoholPercentage) / 100 + (student2.alcoholVolume * student2.alcoholicBeverage.alcoholPercentage) / 100 + (student3.alcoholVolume * student3.alcoholicBeverage.alcoholPercentage) / 100 + (student4.alcoholVolume * student4.alcoholicBeverage.alcoholPercentage) / 100 + (student5.alcoholVolume * student5.alcoholicBeverage.alcoholPercentage) / 100;
+decimal volumeAlkohol = (student1.alcoholVolume * student1.alcoholicBeverage.alcoholPercentage) / 100 + 
+    (student2.alcoholVolume * student2.alcoholicBeverage.alcoholPercentage) / 100 + 
+    (student3.alcoholVolume * student3.alcoholicBeverage.alcoholPercentage) / 100 + 
+    (student4.alcoholVolume * student4.alcoholicBeverage.alcoholPercentage) / 100 + 
+    (student5.alcoholVolume * student5.alcoholicBeverage.alcoholPercentage) / 100;
 Console.WriteLine($"Общий объём алкоголя: {volumeAlkohol}");
 
-Console.WriteLine($"{student1.firstName} {student2.lastName}: выпил {(student1.alcoholVolume / volume) * 100}% от общего количества, {(student1.alcoholicBeverage.alcoholPercentage * student1.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
-Console.WriteLine($"{student2.firstName} {student2.lastName}: выпил {(student2.alcoholVolume / volume) * 100}% от общего количества, {(student2.alcoholicBeverage.alcoholPercentage * student2.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
-Console.WriteLine($"{student3.firstName} {student3.lastName}: выпил {(student3.alcoholVolume / volume) * 100}% от общего количества, {(student3.alcoholicBeverage.alcoholPercentage * student3.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
-Console.WriteLine($"{student4.firstName} {student4.lastName}: выпил {(student4.alcoholVolume / volume) * 100}% от общего количества, {(student4.alcoholicBeverage.alcoholPercentage * student4.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
-Console.WriteLine($"{student5.firstName} {student5.lastName}: выпил {(student5.alcoholVolume / volume) * 100}% от общего количества, {(student5.alcoholicBeverage.alcoholPercentage * student5.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
+Console.WriteLine($"{student1.firstName} {student1.lastName}: выпил {(student1.alcoholVolume / volume) * 100}% от общего количества," +
+    $" {(student1.alcoholicBeverage.alcoholPercentage * student1.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
+Console.WriteLine($"{student2.firstName} {student2.lastName}: выпил {(student2.alcoholVolume / volume) * 100}% от общего количества," +
+    $" {(student2.alcoholicBeverage.alcoholPercentage * student2.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
+Console.WriteLine($"{student3.firstName} {student3.lastName}: выпил {(student3.alcoholVolume / volume) * 100}% от общего количества," +
+    $" {(student3.alcoholicBeverage.alcoholPercentage * student3.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
+Console.WriteLine($"{student4.firstName} {student4.lastName}: выпил {(student4.alcoholVolume / volume) * 100}% от общего количества, " +
+    $"{(student4.alcoholicBeverage.alcoholPercentage * student4.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
+Console.WriteLine($"{student5.firstName} {student5.lastName}: выпил {(student5.alcoholVolume / volume) * 100}% от общего количества, " +
+    $"{(student5.alcoholicBeverage.alcoholPercentage * student5.alcoholVolume) / volumeAlkohol}% от общего количества спирта");
 
 
 
